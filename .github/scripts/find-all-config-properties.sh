7fedc3b9db42b3e4ae335ac320e050c016538a4b
Dave Harlan <david.harlan@seeq.com>
2023-06-30 11:11:49 -0400

    Remove actions configuration

--- .github/scripts/find-all-config-properties.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-grep -Pohr --include '*.java' --exclude-dir=test \"otel.instrumentation.[^\"]+\" \
-  | grep -v otel.instrumentation.internal \
-  | sort -u
--- .github/scripts/get-version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-grep -Po "val stableVersion = \"\K[0-9]+.[0-9]+.[0-9]+" version.gradle.kts
